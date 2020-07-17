4047078a1b245b28a8095ccd79c7ac49c0612307
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-07-17 19:55:34 +0530

    Update the prereqs script

--- 00-prereqs.sh
+++ 00-prereqs.sh
@@ -43,3 +43,12 @@ fi
 
 echo ">>> Is jq (https://stedolan.github.io/jq/) installed?"
 jq -V
+
+echo ">>> Looking for plugin updates"
+plugins_that_need_update=$(ibmcloud plugin list --output json | jq '[.[]|select(.Status=="Update Available")]|length')
+if [ $plugins_that_need_update -gt 0 ]; then
+  ibmcloud plugin list
+  echo
+  echo "WARNING $plugins_that_need_update plugins need updating.  Update them by executing:"
+  echo ibmcloud plugin update --all
+fi
