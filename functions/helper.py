a0b8aeaf7e4611a268ade35dee402c2b9253618b
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-07-23 17:16:14 +0530

    Cleanup

--- functions/helper.py
+++ functions/helper.py
@@ -15,12 +15,6 @@ def helper(params):
 
 # To extract JSON objects from the incoming activity tracker with LogDNA alert
 def extract_json_objects(text, decoder=JSONDecoder()):
-    """Find JSON objects in text, and yield the decoded JSON data
-
-    Does not attempt to look for JSON arrays, text, or other JSON types outside
-    of a parent JSON object.
-
-    """
     pos = 0
     while True:
         match = text.find('{', pos)
@@ -36,7 +30,6 @@ def extract_json_objects(text, decoder=JSONDecoder()):
 # Get the network interface ID associated with an Instance(VSI)
 def get_network_interface_id(instance_Id):
     # Get instance
-    # print("Get Instance")
     try:
         instance = service.get_instance(
         id=instance_Id
@@ -44,29 +37,21 @@ def get_network_interface_id(instance_Id):
     except ApiException as e:
         print("Get instance failed with status code " +
           str(e.code) + ": " + e.message)
-    # print("INSTANCE details",instance)
-    # print("NETWORK INSTANCE ID is",instance['network_interfaces'][0]['id'])
     return instance['network_interfaces'][0]['id']
 
-# Get an unbound floating IP or create a new floating IP
+# Get an unbound floating IP. If none exists, create a new floating IP
 def get_floating_ip_id(fip_name,zone):
-    #print("FIP_NAME",fip_name)
     print("List Floating IPs")
     try:
         floating_ips = service.list_floating_ips().get_result()['floating_ips']
-        # print(floating_ips)
     except ApiException as e:
         print("List floating IPs failed with status code " + str(e.code) + ": " + e.message)
-    #filter(lambda x : x['status'] == 'available', floating_ips)
     unassigned_fip_List=[]
     floating_ip_id=""
     key='target'
     for floating_ip in floating_ips:
-        #print("KEYS",floating_ip.keys())
         if key not in floating_ip:
             unassigned_fip_List.append(floating_ip['id'])
-        #print(floating_ip['id'], "\t",  floating_ip['name'])
-    #print("UNASSIGNED:",unassigned_fip_List)
     if not unassigned_fip_List:
         zone_identity_model = {}
         zone_identity_model['name'] = zone
@@ -83,7 +68,6 @@ def get_floating_ip_id(fip_name,zone):
 
         # Invoke method
         print("Reserve Floating IP")
-        #print(floating_ip_prototype)
         try:
             ip = service.create_floating_ip(
                 floating_ip_prototype=floating_ip_prototype, headers={}).get_result()
@@ -101,7 +85,6 @@ def get_floating_ip_id(fip_name,zone):
 def add_instance_floating_ip(instance_id, network_interface_id,zone):
     # Reserve a floating IP
     floating_ip_id=get_floating_ip_id(instance_id.split('-')[-1]+"-floating-ip",zone)
-    #print("Floating IP name:",instance_id.split('-')[-1]+"-floating-ip")
     try:
         response = service.add_instance_network_interface_floating_ip(
         instance_id=instance_id,
@@ -110,7 +93,6 @@ def add_instance_floating_ip(instance_id, network_interface_id,zone):
         ).get_result()
     except ApiException as e:
         print("Attaching floating IP to the instance failed with status code " + str(e.code) + ": " + e.message)
-    #print(response)
     return response
 
 # Get the instance ID of the provisioned VSI
@@ -121,12 +103,9 @@ def get_instance_id(dict):
             instance_Id=result['_app'][result['_app'].find('instance:')+9:]
             instance_Id=instance_Id.replace(" ","")
             zone=result['_app'].split(':')[5]
-            print("The instance ID is",instance_Id)
             network_interface_id=get_network_interface_id(instance_Id)
             response=add_instance_floating_ip(instance_Id,network_interface_id,zone)
-            #print("FIP attached RESPONSE",response)
             finalResponse.append(response)
         except:
             continue
-    #print("FINAL response",finalResponse)
     return finalResponse
